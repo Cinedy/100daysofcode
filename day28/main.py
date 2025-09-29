a4453fa381084a125b7cc46be697a6db8174911a
Silje Melling <silje.melling94@gmail.com>
2025-09-29 21:13:21 +0200

    small fixes to day28

--- day28/main.py
+++ day28/main.py
@@ -1,7 +1,8 @@
+#POMODORO APP
+
 from tkinter import *
 import math
 
-from numpy import long
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,25 +14,38 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)  
+    checkmark_label.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
     global reps
+    reps += 1
+
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    if reps % 2 != 0:
-        count_down(work_sec)
-    elif reps % 8 == 0:
+    if reps % 8 == 0:
         count_down(long_break_sec)
-    else:
+        title_label.config(text="Break", fg=RED)  
+    elif reps % 2 == 0:        
         count_down(short_break_sec)
-    reps += 1
-    count_down(5)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -45,9 +59,16 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        checkmark_label.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -64,13 +85,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(106, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
-checkmark_label = Label(text="✔", font=(FONT_NAME, 15), fg=GREEN, bg=YELLOW)
+checkmark_label = Label(font=(FONT_NAME, 15), fg=GREEN, bg=YELLOW)
 checkmark_label.grid(column=1, row=3)
 
 start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"), bg="#FFFFFF", command=start_timer)
 start_button.grid(column=0,row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"), bg="#FFFFFF") #, command=restart_timer
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"), bg="#FFFFFF", command=reset_timer)
 reset_button.grid(column=2,row=2)
 
 
