5d5123007e0b80a1847caa9f30bd635a95db2697
Silje Melling <silje.melling94@gmail.com>
2025-09-29 20:58:53 +0200

    cleaned up day26

--- day26/main.py
+++ day26/main.py
@@ -1,35 +1,11 @@
-# List comprehension = create a new list from a previous list
-# new list = [new_item for item in list]
+# List comprehension = create a new list from a previous list : new list = [new_item for item in list]
+# Keyword Method with iterrows() : {new_key:new_value for (index, row) in df.iterrows()}
 
 #NATO ALPHABET PROJECT
-
-
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    # print(key, value)
-    pass
-
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # print(row.student + ":" + " " + str(row.score))
-    #Access index and row
-    #Access row.student or row.score
-    pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (index, row) in df.iterrows()}
@@ -37,6 +13,14 @@ phonetic_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output = [phonetic_dict[letter] for letter in word]
-print(output)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters in the alphabet!")
+        generate_phonetic()
+    else: 
+        print(output)
+
+generate_phonetic()
